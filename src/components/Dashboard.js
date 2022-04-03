dfced3a4dcbd7c18b0a9dc5c6a6eec4879f32e14
sajjadhira <sajjad.hira12@gmail.com>
2022-04-04 05:49:03 +0600

    rechart integrated

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,23 @@
 import { Card } from "react-bootstrap";
 import { useEffect, useState } from "react";
 
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
 const Dashboard = () => {
+  const data = [
+    { name: "Page A", uv: 400, pv: 2400, amt: 2400 },
+    { name: "Page B", uv: 350, pv: 2400, amt: 2400 },
+    { name: "Page C", uv: 150, pv: 2400, amt: 2400 },
+    { name: "Page D", uv: 300, pv: 2400, amt: 2400 },
+  ];
   return (
     <>
       <div className="row">
@@ -45,6 +61,27 @@ const Dashboard = () => {
                 the bulk of the card's content.
               </Card.Text>
             </Card.Body>
+          </Card>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-6">
+          <Card className="mt-3 pb-5 mb-3">
+            <h4 className="ms-3 mt-2">Sales</h4>
+            <ResponsiveContainer width="95%" height={400}>
+              <LineChart
+                className="pt-2 pb-5"
+                data={data}
+                margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+              >
+                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+              </LineChart>
+            </ResponsiveContainer>
           </Card>
         </div>
       </div>
